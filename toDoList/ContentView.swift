f42b84224b08ad0d7a8ecad4b4661f85405d1ec4
N0ah610 <55193426+N0ah610@users.noreply.github.com>
2020-02-19 08:10:20 -0500

    Add files via upload

---
+++ toDoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  toDoList
+//
+//  Created by Noah Voelkel (student HH) on 2/19/20.
+//  Copyright © 2020 Noah Voelkel (student HH). All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ toDoList/ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  toDoList
+//
+//  Created by Noah Voelkel (student HH) on 2/19/20.
+//  Copyright © 2020 Noah Voelkel (student HH). All rights reserved.
+//
+
+import Foundation
+
+struct ToDo {
+    var Event: String
+    var isCompleted: Bool
+    var dueDate: Date
+    var notes: String?
+}
